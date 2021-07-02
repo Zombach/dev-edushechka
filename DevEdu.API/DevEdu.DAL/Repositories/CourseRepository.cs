7087366578b4819b8238e332008762ffecba5b0d
Aleks <a>
2021-07-02 18:17:24 +0300

    added course,user repo

---
+++ DevEdu.API/DevEdu.DAL/Repositories/CourseRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using DevEdu.DAL.Models;
+
+public class CouseRepository
+{
+    private static string _connectionString =
+        "Data Source=80.78.240.16;Initial Catalog = DevEdu; Persist Security Info=True;User ID = student;Password=qwe!23;" +
+        " Pooling=False;MultipleActiveResultSets=False;Connect Timeout = 60; Encrypt=False;TrustServerCertificate=False";
+    private IDbConnection _connection = new SqlConnection(_connectionString) ;
+    public void AddCourseMaterial(int courseId, int materialId)
+    {
+        _connection.Query("[dbo].[Course_Material_Insert]", new
+        {
+            courseId,
+            materialId
+        },
+            commandType: CommandType.StoredProcedure);
+    }
+
+    public void RemoveCourseMaterial(int courseId, int materialId)
+    {
+        _connection.Query("dbo.Course_Material_Delete", new
+        {
+            courseId,
+            materialId
+        },
+            commandType: CommandType.StoredProcedure);
+    }
+
+    public void AddCourseTask(int courseId, int taskId)
+    {
+        _connection.Query("[dbo].[Course_Task_Insert]", new
+        {
+            courseId,
+            taskId
+        },
+            commandType: CommandType.StoredProcedure);
+    }
+
+    public void RemoveCourseTask(int courseId, int taskId)
+    {
+        _connection.Query("dbo.Course_Task_Delete", new
+        {
+            courseId,
+            taskId
+        },
+            commandType: CommandType.StoredProcedure);
+    }
+}
--- DevEdu.API/DevEdu.DAL/Repositories/UserRepository.cs
+++ DevEdu.API/DevEdu.DAL/Repositories/UserRepository.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using DevEdu.DAL.Models;
 
 namespace DevEdu.DAL.Repositories
 {
     public class UserRepository
     {
+        private static string _connectionString =
+        "Data Source=80.78.240.16;Initial Catalog = DevEdu; Persist Security Info=True;User ID = student;Password=qwe!23;" +
+        " Pooling=False;MultipleActiveResultSets=False;Connect Timeout = 60; Encrypt=False;TrustServerCertificate=False";
+
+        private IDbConnection _connection = new SqlConnection(_connectionString);
+
+        public void AddUserRole(int userId, int roleId)
+        {
+            _connection.Query("[dbo].[User_Role_Insert]", new
+            {
+                userId,
+                roleId
+            },
+                commandType: CommandType.StoredProcedure);
+        }
+
+        public void RemoveUserRole(int userId, int roleId)
+        {
+            _connection.Query("dbo.User_Role_Delete", new
+            {
+                userId,
+                roleId
+            },
+                commandType: CommandType.StoredProcedure);
+        }
     }
 }
